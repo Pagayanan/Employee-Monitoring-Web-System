26a3872d5a6b0a8a430fa5bd20e0f09d4af19e78
Pagayanan <159064662+Pagayanan@users.noreply.github.com>
2024-10-31 09:23:13 +0800

    Add files via upload

---
+++ Admin/Application_For_Leave_revised/exp.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YouTube Embedded Video</title>
+    <style>
+        .center {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+    </style>
+</head>
+<body>
+    <div class="center">
+    <iframe width="600" height="400" src="https://www.wikipedia.org" frameborder="0"></iframe>
+    </div>
+</body>
+</html>
